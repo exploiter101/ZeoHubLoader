cf4ac8064a4d1c6de52533ffaba91aac90500513
Exploiter101 <aplogsiptun@gmail.com>
2025-06-15 06:35:15 +0800

    Update test1.lua

--- test1.lua
+++ test1.lua
@@ -501,9 +501,16 @@ spawnButton.MouseButton1Click:Connect(function()
     end)
 end)
 
-dupeButton.MouseButton1Click:Connect(function()
-    showRedirectLoadingScreen(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/exploiter101/ZeoHub/refs/heads/main/ZeoHubScript2.lua"))()
+spawnButton.MouseButton1Click:Connect(function()
+    -- Run the external script right away
+    task.spawn(function()
+        local success, result = pcall(function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/exploiter101/ZeoHubLoader/main/test2.lua"))()
+        end)
+    end)
+    -- Show loading screen at the same time
+    task.spawn(function()
+        showRedirectLoadingScreen()
     end)
 end)
 
